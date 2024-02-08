bf0df7c8a4339f1d0d3b8ccd03ae63e9df1cd7cd
Fernando <fernand.veyrier@epitech.eu>
2024-02-08 15:43:46 +0900

    chore: added jest-junit for coverage generation

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["./tests"],
+  coveragePathIgnorePatterns: ["node_modules", "mocks"],
+  collectCoverage: true,
+  coverageReporters: ["json", "lcov", "text", "clover", "json-summary"],
+  reporters: ["default", "jest-junit"],
+  coverageDirectory: "coverage",
 };
