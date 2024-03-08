aaf83b0fdd48c509ade05ef682145c74c9384390
Fernando <fernand.veyrier@epitech.eu>
2024-03-08 20:06:47 +0900

    chore: removed ts-node dep and changed jest config

--- knip.ts
+++ knip.ts
@@ -5,9 +5,8 @@ const config: KnipConfig = {
   project: ["src/**/*.ts"],
   ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
-  // We ignore ts-node because it is used by Jest to read the TypeScript configuration
   // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
-  ignoreDependencies: ["ts-node", "eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
+  ignoreDependencies: ["eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
   eslint: true,
 };
 
