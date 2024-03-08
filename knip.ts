9222b7168e6ce936bdfbf55dec99f784470ebb3c
Fernando <fernand.veyrier@epitech.eu>
2024-03-08 19:45:47 +0900

    chore: added ignore rules to knip

--- knip.ts
+++ knip.ts
@@ -3,10 +3,12 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   entry: ["build/index.ts"],
   project: ["src/**/*.ts"],
-  ignore: ["src/types/config.ts"],
+  ignore: ["src/types/config.ts", "**/__mocks__/**", "**/__fixtures__/**"],
   ignoreExportsUsedInFile: true,
   // We ignore ts-node because it is used by Jest to read the TypeScript configuration
-  ignoreDependencies: ["ts-node"],
+  // eslint can also be safely ignored as per the docs: https://knip.dev/guides/handling-issues#eslint--jest
+  ignoreDependencies: ["ts-node", "eslint-config-prettier", "eslint-plugin-prettier", "@types/jest", "@mswjs/data"],
+  eslint: true,
 };
 
 export default config;
