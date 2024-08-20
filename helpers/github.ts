2ddf2890b3e2e4cfd39621f65a0fef5946dfaf30
jordan-ae <arreyetta1jnr@gmail.com>
2024-08-20 14:05:41 +0100

    test

--- helpers/github.ts
+++ helpers/github.ts
@@ -555,7 +555,7 @@ async function applyStateChanges(projectIssues: GitHubIssue[], projectIssue: Git
     },
     // it's open, unassigned, has price labels and is closed in the devpool
     issueUnassigned_Open: {
-      cause: projectIssue.state === "open" && devpoolIssue.state === "closed" && !projectIssue.assignee?.login && !hasNoPriceLabels && !isAuthorizedCreator,
+      cause: projectIssue.state === "open" && devpoolIssue.state === "open" && !projectIssue.assignee?.login && !hasNoPriceLabels && !isAuthorizedCreator,
       effect: "closed",
       comment: "Reopened (unassigned)",
     },
