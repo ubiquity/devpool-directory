2a50c7b3eae606a11355ea141b4b13d4bc055a35
jordan-ae <arreyetta1jnr@gmail.com>
2024-08-22 05:53:40 +0100

    cleanup

--- scripts/delete-unauthorized-issues.sh
+++ scripts/delete-unauthorized-issues.sh
@@ -3,7 +3,7 @@
 REPO="ubiquity/devpool-directory"
 AUTHORIZED_ORG_IDS=(76412717 133917611 165700353)
 
-# Fetch issues with author login and author association (organization info might be absent)
+# Fetch issues with author login and author association
 issues=$(gh issue list --repo "$REPO" --limit 100 --json number,author,title)
 
 # Check if issues JSON is valid
