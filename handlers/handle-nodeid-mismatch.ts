fd9930796a858b4e680eacea60e33090dec0526a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-21 23:29:14 +0100

    chore: node id mismatch

---
+++ handlers/handle-nodeid-mismatch.ts
@@ -0,0 +1,183 @@
+import { octokit, DEVPOOL_OWNER_NAME, DEVPOOL_REPO_NAME } from "../helpers/github";
+import { getIssueLabelValue, getIssueByLabel } from "../helpers/issue";
+import { GitHubIssue } from "../types/github";
+import { graphql } from "@octokit/graphql";
+
+export async function handleNodeIdMismatches(
+    devpoolIssue: GitHubIssue,
+    projectMap: Map<string, GitHubIssue>,
+    devpoolIssues: GitHubIssue[],
+    missingPartnerTasks: Set<string>,
+    deletedPartnerTasks: Set<string>
+) {
+    let partnerTaskId = getIssueLabelValue(devpoolIssue, "id:");
+    if (!partnerTaskId) {
+        // we should delete these issues as they are most likely unofficial bots
+        return;
+    }
+    /**
+     * If the task is not in the project map, then we need to fetch it
+     * although it should be because the map consists of only task
+     * node ids that are present in the project issues
+     */
+    if (!projectMap.has(partnerTaskId)) {
+        try {
+            // using the node ID, fetch whatever issue it is
+            const taskViaGraphQL = await fetchIssueByNodeId(partnerTaskId);
+            if (taskViaGraphQL) {
+                const taskViaRest = await octokit.rest.issues.get({
+                    owner: DEVPOOL_OWNER_NAME,
+                    repo: DEVPOOL_REPO_NAME,
+                    issue_number: taskViaGraphQL.number,
+                });
+                projectMap.set(taskViaRest.data.node_id, taskViaRest.data);
+            } else {
+                await handleMissingTask(partnerTaskId, devpoolIssues, projectMap, missingPartnerTasks);
+            }
+        } catch (error) {
+            let message = "";
+
+            if (typeof error === "string") {
+                message = error;
+            } else if (error instanceof Error) {
+                message = error.message;
+            } else if (error && typeof error === "object" && "response" in error) {
+                const err = error as { response: { url: string, status: number, data: { message: string } } };
+                message = err.response.data.message;
+            } else {
+                console.error(`Error fetching task with node ID4 ${partnerTaskId}`, error);
+            }
+
+            if (message.includes("This issue was deleted")) {
+                deletedPartnerTasks.add(partnerTaskId);
+            }
+
+            missingPartnerTasks.add(partnerTaskId);
+        }
+    }
+
+
+    /**
+     * These tasks could not be found by either the project map or the GraphQL fetch
+     * so we need to refetch the issue using the task URL from the devpool body
+     */
+    if (missingPartnerTasks.has(partnerTaskId)) {
+        await handleMissingTask(partnerTaskId, devpoolIssues, projectMap, missingPartnerTasks);
+    }
+}
+
+export async function handleMissingTask(
+    partnerTaskId: string,
+    devpoolIssues: GitHubIssue[],
+    projectMap: Map<string, GitHubIssue>,
+    missingPartnerTasks: Set<string>
+) {
+    // use the old partnerTaskId to find the issue amongst the devpool issues
+    const devpoolIssue = getIssueByLabel(devpoolIssues, `id: ${partnerTaskId}`);
+    // this should always be true
+    if (devpoolIssue) {
+        const match = devpoolIssue.body?.match(/https:\/\/github.com\/(?<owner>[^/]+)\/(?<repo>[^/]+)\/issues\/(?<number>\d+)/);
+
+        if (match?.groups) {
+            const { owner, repo, number } = match.groups;
+            let issue;
+
+            try {
+                issue = await octokit.rest.issues.get({
+                    owner,
+                    repo,
+                    issue_number: parseInt(number),
+                });
+            } catch (e) {
+                console.error(`Error fetching issue with number ${number}:`, e);
+            }
+
+            if (!issue || !issue.data || issue.status !== 200) {
+                // if no issue is found, we should probably delete these as
+                // most likely org/owner/repo/issue has been deleted or moved.
+                return;
+            }
+
+            const { node_id, html_url, title } = issue.data;
+            // update the project map with the new node ID
+            projectMap.set(node_id, issue.data);
+            // remove the task from the missing set
+            missingPartnerTasks.delete(partnerTaskId);
+
+            console.log(`Found missing task with node ID ${partnerTaskId} and updated the project map with the new node ID ${node_id}`, {
+                partnerTaskId,
+                node_id,
+                html_url,
+                title
+            });
+
+            // update the devpool issue with the new node ID
+            await octokit.rest.issues.updateLabel({
+                owner: DEVPOOL_OWNER_NAME,
+                repo: DEVPOOL_REPO_NAME,
+                issue_number: devpoolIssue.number,
+                current_name: `id: ${partnerTaskId}`,
+                name: `id: ${node_id}`,
+            });
+        } else {
+            console.log(`Attempted to locate issue after failing to find it in the project map, but failed to find it in the devpool body`, {
+                partnerTaskId,
+                devpoolIssueUrl: devpoolIssue.html_url,
+                devpoolIssueBody: devpoolIssue.body
+            });
+        }
+    } else {
+        console.log(`Found a task which has an incorrect partnerTaskId, but could not find the issue in the devpool issues`, {
+            partnerTaskId,
+            devpoolIssue
+        });
+    }
+}
+
+export async function fetchIssueByNodeId(nodeId: string) {
+    type IssueNode = {
+        __typename: string;
+        id: string;
+        number: number;
+        title: string;
+        body: string;
+        state: string;
+        url: string;
+    };
+
+    type IssueNodeResponse = {
+        node: IssueNode;
+    };
+
+    const query = `
+    query($nodeId: ID!) {
+      node(id: $nodeId) {
+        ... on Issue {
+          __typename
+          id
+          number
+          title
+          body
+          state
+          url
+        }
+      }
+    }
+  `;
+
+    try {
+        const result = await graphql<IssueNodeResponse>(query, {
+            nodeId,
+            headers: {
+                authorization: `token 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`,
+            },
+        });
+
+        return result.node;
+    } catch (error) {
+        if (typeof error === "object" && error && "errors" in error) {
+            const err = error as { errors: { message: string }[] };
+            console.error(`Error fetching issue with node ID ${nodeId}:`, err.errors);
+        }
+    }
+}
--- helpers/get-project-map.ts
+++ helpers/get-project-map.ts
@@ -1,4 +1,6 @@
+import { handleNodeIdMismatches } from "../handlers/handle-nodeid-mismatch";
 import { GitHubIssue } from "../types/github";
+import { getIssueByLabel } from "./issue";
 import { getRepoCredentials } from "./repos";
 
 export async function getProjectMap(devpoolIssues: GitHubIssue[], allProjectIssues: GitHubIssue[]) {
@@ -8,6 +10,30 @@ export async function getProjectMap(devpoolIssues: GitHubIssue[], allProjectIssu
 
   console.log(`Found ${projectMap.size} project issues in total`);
   console.log(`Found ${devpoolIssues.length} devpool issues in total`);
+
+
+  // a set of extracted node IDs from devpool issues that are not found in the project map
+  const missingPartnerTasks = new Set<string>();
+  const deletedPartnerTasks = new Set<string>();
+
+  // align any devpool issues which have node ID labels that do not match the project issue
+  for (const devpoolIssue of devpoolIssues) {
+    await handleNodeIdMismatches(devpoolIssue, projectMap, devpoolIssues, missingPartnerTasks, deletedPartnerTasks);
+  }
+
+  if (missingPartnerTasks.size > 0) {
+    console.log(`There are ${missingPartnerTasks.size} missing partner tasks`, {
+      missingPartnerTasks: [...missingPartnerTasks],
+      missingPartnerUrls: [...missingPartnerTasks].map(partnerTaskId => getIssueByLabel(devpoolIssues, `id: ${partnerTaskId}`)?.html_url)
+    });
+  }
+
+  if (deletedPartnerTasks.size > 0) {
+    console.log(`There are ${deletedPartnerTasks.size} deleted partner tasks`, {
+      deletedPartnerTasks: [...deletedPartnerTasks],
+      deletedPartnerUrls: [...deletedPartnerTasks].map(partnerTaskId => getIssueByLabel(devpoolIssues, `id: ${partnerTaskId}`)?.html_url)
+    });
+  }
 
   return projectMap;
 }
