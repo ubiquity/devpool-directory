c93eb2b375b707cfb3bd230448b42fd56369afdc
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-21 19:22:22 +0100

    chore: refactor index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import dotenv from "dotenv";
-import {
-  DEVPOOL_OWNER_NAME,
-  DEVPOOL_REPO_NAME,
-} from "./helpers/github";
+import { DEVPOOL_OWNER_NAME } from "./helpers/github";
 import { readFile, writeFile } from "fs/promises";
 import { Statistics } from "./types/statistics";
 import { createDevPoolIssue } from "./handlers/create-devpool-issue";
 import { handleDevPoolIssue } from "./handlers/handle-devpool-issue";
-import { getAllIssues, getIssueByLabel } from "./helpers/issue";
+import { getAllIssues, getIssueByLabel, getIssueLabelValue } from "./helpers/issue";
 import { getProjectUrls, getRepoCredentials } from "./helpers/repos";
 import { calculateStatistics, writeTotalRewardsToGithub } from "./helpers/statistics";
 import { checkIfForked } from "./helpers/utils";
 import { GitHubIssue } from "./types/github";
+import { getAllDevpoolIssues } from "./helpers/get-all-devpool-issues";
+import { getProjectMap } from "./helpers/get-project-map";
 // init octokit
 dotenv.config();
 
@@ -31,15 +30,17 @@ async function main() {
     await writeFile("./twitterMap.json", JSON.stringify({}));
   }
 
-  // get devpool issues
-  const devpoolIssues: GitHubIssue[] = await getAllIssues(DEVPOOL_OWNER_NAME, DEVPOOL_REPO_NAME);
-
   // aggregate projects.urls and opt settings
   const projectUrls = await getProjectUrls();
+
+  // get devpool issues
+  const devpoolIssues: GitHubIssue[] = await getAllDevpoolIssues(projectUrls);
 
   // aggregate all project issues
   const allProjectIssues: GitHubIssue[] = [];
 
+  // check if the devpool is a fork
+  // (if this _is_ a fork, update your DEVPOOL_OWNER_NAME to your org name)
   const isFork = await checkIfForked(DEVPOOL_OWNER_NAME);
 
   // for each project URL
@@ -50,29 +51,44 @@ async function main() {
     const projectIssues: GitHubIssue[] = await getAllIssues(ownerName, repoName);
     // aggregate all project issues
     allProjectIssues.push(...projectIssues);
+  }
+
+  const projectMap = await getProjectMap(devpoolIssues, allProjectIssues);
+
+  // for all issues
+  for (const projectIssue of projectMap.values()) {
+    // if issue exists in devpool
+    const devpoolIssue = getIssueByLabel(devpoolIssues, `id: ${projectIssue.node_id}`);
+    const projectHtmlUrl = projectIssue.html_url;
+    const ownerRepo = getRepoCredentials(projectHtmlUrl).join("/");
+
+    // adding www creates a link to an issue that does not count as a mention
+    // helps with preventing a mention in partner's repo especially during testing
+    const body = isFork ? projectHtmlUrl.replace("https://github.com", "https://www.github.com") : projectHtmlUrl;
+
     // for all issues
-    for (const projectIssue of projectIssues) {
-      // if issue exists in devpool
-      const devpoolIssue = getIssueByLabel(devpoolIssues, `id: ${projectIssue.node_id}`);
+    if (devpoolIssue) {
+      const partnerTaskId = getIssueLabelValue(devpoolIssue, "id:");
+      const isMissingInPartners = partnerTaskId ? !projectMap.has(partnerTaskId) : true;
 
-      // adding www creates a link to an issue that does not count as a mention
-      // helps with preventing a mention in partner's repo especially during testing
-      const body = isFork ? projectIssue.html_url.replace("https://github.com", "https://www.github.com") : projectIssue.html_url;
-
-      // for all issues
-      if (devpoolIssue) {
-        // if it exists in the devpool, then update it
-        await handleDevPoolIssue(projectIssues, projectIssue, projectUrl, devpoolIssue, isFork);
-      } else {
-        // if it doesn't exist in the devpool, then create it
-        await createDevPoolIssue(projectIssue, projectUrl, body, twitterMap);
+      if (isMissingInPartners) {
+        console.log(`Partner task not found in project map`, {
+          partnerTaskId,
+          projectIssue: projectHtmlUrl,
+          devpoolIssue: devpoolIssue.html_url,
+        });
       }
+
+      // if it exists in the devpool, then update it
+      await handleDevPoolIssue(isMissingInPartners, projectIssue, ownerRepo, devpoolIssue, isFork);
+    } else {
+      // if it doesn't exist in the devpool, then create it
+      await createDevPoolIssue(projectIssue, ownerRepo, body, twitterMap);
     }
   }
 
   // Calculate total rewards from devpool issues
-  const { rewards, tasks } = await calculateStatistics(await getAllIssues(DEVPOOL_OWNER_NAME, DEVPOOL_REPO_NAME));
-  const statistics: Statistics = { rewards, tasks };
+  const statistics: Statistics = await calculateStatistics(await getAllDevpoolIssues(projectUrls), projectMap);
 
   await writeTotalRewardsToGithub(statistics);
 }
