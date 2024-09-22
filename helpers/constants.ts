23e04e16e5035fc5970f47d7e40bdbe3d2df3fa3
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-22 02:11:20 +0100

    chore: name change

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -10,7 +10,7 @@ import _projects from "../projects.json";
  * - number
  */
 export const DEVPOOL_TASK_BODY_REGEX = /https:\/\/(www\.)?github.com\/(?<owner>[^/]+)\/(?<repo>[^/]+)\/issues\/(?<number>\d+)/;
-export const DEVPOOL_OWNER_NAME = "ubiquity";
+export const DEVPOOL_OWNER_NAME = "ubq-testing";
 export const DEVPOOL_REPO_NAME = "devpool-directory";
 
 export const octokit = new Octokit({ auth: process.env.DEVPOOL_GITHUB_API_TOKEN });
