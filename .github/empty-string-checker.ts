9c5da5e6766a45621f150069e042e3058a7172a1
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-09 07:26:46 +0900

    fix: ensure no linkback

--- .github/empty-string-checker.ts
+++ .github/empty-string-checker.ts
@@ -83,7 +83,7 @@ async function createReview(violations: Array<{ file: string; line: number; cont
   const reviewComments = violations.map((v) => ({
     path: v.file,
     line: v.line,
-    body: `> [!WARNING]\n> Empty string found, consider another approach. [Read more](https://github.com/ubiquity/ts-template/issues/31).`,
+    body: `> [!WARNING]\n> Empty string found, consider another approach. [Read more](https://www.github.com/ubiquity/ts-template/issues/31).`,
   }));
 
   await octokit.pulls.createReview({
