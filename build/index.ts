e956bec9c5f827f90503c2196cfec020862b5372
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-09 09:04:33 +0900

    fix: remove dummy empty strings

--- build/index.ts
+++ build/index.ts
@@ -2,6 +2,3 @@ import * as dotenv from "dotenv";
 // load environment variables (if you have them)
 dotenv.config();
 console.log("Welcome to ts-template");
-
-const _EMPTY_STRING_TEST_1 = "";
-const _EMPTY_STRING_TEST_2 = "";
